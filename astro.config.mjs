1eadeffbb3c30204f687607212d106a609d66bff
web-luisaguilar <web.luisaguilar@gmail.com>
2025-06-10 11:55:07 -0600

    v2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Poppins',
+        cssVariable: '--font-poppins'
+      }
+    ]
+  }
+})
